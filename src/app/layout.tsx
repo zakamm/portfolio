ab27d5e68d4a788741bfc2c19c3cfcee3de755c0
zakamm <mohidzaka3@gmail.com>
2025-06-03 18:30:22 -0400

    add posthog + proxy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Roboto_Mono } from "next/font/google";
 import "./globals.css";
+import { PostHogProvider } from "./providers";
 
 const robotoMono = Roboto_Mono({
   subsets: ["latin"],
@@ -38,7 +39,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${robotoMono.className} antialiased`}>{children}</body>
+      <body className={`${robotoMono.className} antialiased`}>
+        <PostHogProvider>{children}</PostHogProvider>
+      </body>
     </html>
   );
 }
